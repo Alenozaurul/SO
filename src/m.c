a4a471ef93ea2c420186449b6028ae566f140986
Alenozaurul <alenozaurul@gmail.com>
2026-05-19 01:31:51 +0300

    m.c works fully now

--- src/m.c
+++ src/m.c
@@ -127,7 +127,7 @@ void printReport(Report report) {
 	printf("Report id: %i\n", report.id);
 	printf("Username: %s\n", report.username);
 	printf("Coordinates: %.2f, %.2f\n", report.x, report.y);
-	printf("Category: %s", report.category);
+	printf("Category: %s\n", report.category);
 	printf("Severity level: %i\n", report.level);
 	printf("Timestamp: %s", ctime(&report.timestamp));
 	printf("Description: %s\n\n", report.description);
@@ -147,7 +147,6 @@ void addReport(){
 	if(!checkRole("inspector")) {
 		char msg[256];
 		sprintf(msg, "User %s tried to add a report for district %s\n", username, district);
-		lseek(log_file, 0, SEEK_END);
 		write(log_file, &msg, strlen(msg));	
 
 		return;
@@ -155,7 +154,7 @@ void addReport(){
 
 	Report report;
 	printf("Category: ");
-	fgets(report.category, sizeof(report.category), stdin);
+	scanf("%s", report.category);
 	printf("Severity level: ");
 	scanf("%i", &report.level);
 	printf("X, Y: ");
@@ -182,7 +181,6 @@ void addReport(){
 
 	char msg[256];
 	sprintf(msg, "Report %d has been added to district %s by %s\n", report.id, district, report.username);
-	lseek(log_file, 0, SEEK_END);
 	write(log_file, &msg, strlen(msg));
 }
 
@@ -190,7 +188,6 @@ void listReports() {
 	if(!checkRole("inspector")) {
 		char msg[256];
 		sprintf(msg, "User %s tried to list all reports from district %s\n", username, district);
-		lseek(log_file, 0, SEEK_END);
 		write(log_file, &msg, strlen(msg));	
 
 		return;
@@ -225,7 +222,6 @@ void listReports() {
 
 	char msg[256];
 	sprintf(msg, "Reports from district %s have been listed by %s\n", district, report.username);
-	lseek(log_file, 0, SEEK_END);
 	write(log_file, &msg, strlen(msg));	
 }
 
@@ -235,7 +231,6 @@ void viewReport(int argc, char *argv[]) {
 	if(!checkRole("inspector")) {
 		char msg[256];
 		sprintf(msg, "User %s tried to view report %d from district %s\n", username, id, district);
-		lseek(log_file, 0, SEEK_END);
 		write(log_file, &msg, strlen(msg));	
 
 		return;
@@ -267,7 +262,6 @@ void viewReport(int argc, char *argv[]) {
 		char msg[256];
 		sprintf(msg, "User %s tried to view report %d from district %s but id failed\n",
 			       	username, id, district);
-		lseek(log_file, 0, SEEK_END);
 		write(log_file, &msg, strlen(msg));	
        		
 		return;
@@ -277,7 +271,6 @@ void viewReport(int argc, char *argv[]) {
 	
 	char msg[100];
 	sprintf(msg, "Report %d view by %s\n", id, report.username);
-	lseek(log_file, 0, SEEK_END);
 	write(log_file, &msg, strlen(msg));	
 }
 
@@ -310,15 +303,360 @@ void updateTreshold(int argc, char *argv[]) {
 
 	write(district_file, &value, sizeof(value));
 	ftruncate(district_file, sizeof(value));
+	
+	char msg[100];
+	sprintf(msg, "Treshold updated by %s\n", username);
+	write(log_file, &msg, strlen(msg));	
 }
 
-void filterReports(){;}
-void removeReport(){;}
-void removeDistrict(){;}
+int getOperation(char string[]) {
+	char *p = strtok(string, ":");
+	p = strtok(NULL, ":");
+
+	if(!strcmp(p, "=="))
+		return 1;
+	if(!strcmp(p, "!="))
+		return 2;
+	if(!strcmp(p, "<"))
+		return 3;
+	if(!strcmp(p, "<="))
+		return 4;
+	if(!strcmp(p, ">"))
+		return 5;
+	if(!strcmp(p, ">="))
+		return 6;
+
+	return 0;
+}
+
+char *getCondition(char string[]) {
+	return strtok(string, ":");
+}
+
+int handleInt(int op, int number, int cas, Report report) {
+	if(cas == 1) {
+		switch(op) {
+			case (1):
+				if(number == report.timestamp)
+					return 1;
+				break;
+			case (2):
+				if(number != report.timestamp)
+					return 1;
+				break;
+			case (3):
+				if(number > report.timestamp)
+					return 1;
+				break;
+			case (4):
+				if(number >= report.timestamp)
+					return 1;
+				break;
+			case (5):
+				if(number < report.timestamp)
+					return 1;
+				break;
+			case (6):
+				if(number >= report.timestamp)
+					return 1;
+				break;
+			default:
+				return 0;
+		}
+	} else if(cas == 0) {
+		switch(op) {
+			case (1):
+				if(number == report.level)
+					return 1;
+				break;
+			case (2):
+				if(number != report.level)
+					return 1;
+				break;
+			case (3):
+				if(number > report.level)
+					return 1;
+				break;
+			case (4):
+				if(number >= report.level)
+					return 1;
+				break;
+			case (5):
+				if(number < report.level)
+					return 1;
+				break;
+			case (6):
+				if(number <= report.level)
+					return 1;
+				break;
+			default:
+				return 0;
+		}
+	}
+
+	return 0;
+}	
+
+int handleStr(int op, char string[], int cas, Report report) {
+	if(cas == 2) {
+		int n = strcmp(report.username, string);
+		
+		if(op == 1 || op == 3 || op == 6)
+			if(n == 0)
+				return 1;
+		if(op == 3 || op == 4)
+			if(n < 0)
+				return 1;
+		if(op == 5 || op == 6)
+			if(n > 0)
+				return 1;
+		if(op == 2 && n != 0)
+			return 1;	
+	} else if(cas == 3) {
+		int n = strcmp(report.category, string);
+//		printf("\n%i\n", n);
+
+		if(op == 1 || op == 3 || op == 6)
+			if(n == 0)
+				return 1;
+		if(op == 3 || op == 4)
+			if(n < 0)
+				return 1;
+		if(op == 5 || op == 6)
+			if(n > 0)
+				return 1;
+		if(op == 2 && n != 0)
+			return 1;	
+	}
+
+	return 0;
+}
+
+void filterReports(int argc, char *argv[]) {
+	if(!checkRole("inspector")) {
+		char msg[256];
+		sprintf(msg, "User %s tried to filter reports from district %s\n", username, district);
+		write(log_file, &msg, strlen(msg));	
+
+		return;
+	}
+
+	int level = 0;
+	char insp[32] = "";
+	char category[32] = "";
+	time_t timestamp = 0;
+
+	int condition[4] = {0}; // == 1   != 2   < 3   <= 4   > 5    >= 6
+				//  0 = severity level   1 = timestamp   2 = inspector   3 = category
+	for(int i = 1; i < argc - 2; ++i) {
+		if(!strcmp(argv[i], "--filter")) {
+			for(int j = i + 2; j < argc; ++j) {
+				
+				char *repl = strdup(argv[j]);
+				
+				if(!strcmp(getCondition(repl), "severity")) { 
+					char *replace = strdup(argv[j]);
+					condition[0] = getOperation(replace);
+					free(replace);
+
+					char *p = strtok(argv[j], ":");
+					p = strtok(NULL, ":");	
+					p = strtok(NULL, ":");	
+					level = atoi(p);
+
+				}	
+				
+				free(repl);
+				repl = strdup(argv[j]);
+				
+				if(!strcmp(getCondition(repl), "timestamp")) {
+					char *replace = strdup(argv[j]);
+					condition[1] = getOperation(replace);		
+					free(replace);
+
+					char *p = strtok(argv[j], ":");
+					p = strtok(NULL, ":");	
+					p = strtok(NULL, ":");	
+				
+					timestamp = atoi(p);
+
+					/*
+					struct tm tm_time = {0};
+					if(strptime(p, "%Y-%m-%d %H:%M:%S", &tm_time) == NULL) {
+						perror("Cannot set time\n");
+						return;
+					}	
+
+					timestamp = mktime(&tm_time);
+					*/
+				}
+				
+				free(repl);
+				repl = strdup(argv[j]);
+				
+				if(!strcmp(getCondition(repl), "inspector")) { 
+					char *replace = strdup(argv[j]);
+					condition[2] = getOperation(replace);		
+					free(replace);
+
+					char *p = strtok(argv[j], ":");
+					p = strtok(NULL, ":");	
+					p = strtok(NULL, ":");	
+					strcpy(insp, p);	
+				}
+					
+				free(repl);
+				repl = strdup(argv[j]);
+				
+				if(!strcmp(getCondition(repl), "category")) { 
+					char *replace = strdup(argv[j]);
+					condition[3] = getOperation(replace);	
+					free(replace);
+					
+					char *p = strtok(argv[j], ":");
+					p = strtok(NULL, ":");	
+					p = strtok(NULL, ":");	
+					strcpy(category, p);	
+				}	
+				
+				free(repl);
+			}		
+		}
+	}
+	Report report;
+
+	while(read(report_file, &report, sizeof(Report)) == sizeof(Report)) {
+		int show = 1;
+
+		for(int i = 0; i < 4; ++i) {
+			if(!condition[i]) 
+				continue;
 
 
+			switch(i) {
+				case 0:
+					if(!handleInt(condition[i], level, i, report))
+						show = 0;
+					break;	
+				case 1:
+					if(!handleInt(condition[i], timestamp, i, report))
+						show = 0;
+					break;	
+				case 2:
+					if(!handleStr(condition[i], insp, i, report))
+						show = 0;
+					break;	
+				case 3:
+					if(!handleStr(condition[i], category, i, report))
+						show = 0;
+					break;
+				default: ;
+			}	 
+		}
 
+		if(show)
+			printReport(report);
+	}
 
+	printf("abracadabra\n");
+
+	char msg[100];
+	sprintf(msg, "Reports filtered by %s\n", username);
+	write(log_file, &msg, strlen(msg));	
+}
+
+void removeReport(int argc, char *argv[]) {
+	int id;
+	for(int i = 0; i < argc - 2; ++i) {
+		if(!strcmp(argv[i], "--remove_report")) {
+			id = atoi(argv[i + 2]);
+			
+			if(id <= 0) {
+				printf("Invalid id\n");
+				return;
+			}
+
+			break;
+		}
+	}
+
+	if(!checkRole("manager")) {
+		char msg[256];
+		sprintf(msg, "User %s removed report %d from district %s\n", username, id, district);
+		write(log_file, &msg, strlen(msg));	
+
+		return;
+	}
+	
+	Report report;
+	long cursor = -1;
+
+	while(read(report_file, &report, sizeof(Report)) == sizeof(Report)) {
+		if(id == report.id) {
+			cursor = lseek(report_file, -sizeof(Report), SEEK_CUR);
+			break;
+		}
+	}
+
+	if(fstat(report_file, &st) == -1) {
+		perror("Error fstat in removeReport\n");
+		return;
+	}
+
+	if(cursor != -1) {
+		Report next;
+		long sread = cursor + sizeof(Report);
+		long swrite = cursor;
+
+		while(sread < st.st_size) {
+			lseek(report_file, sread, SEEK_SET);
+
+			if(read(report_file, &next, sizeof(Report)) != sizeof(Report)) 
+				break;
+
+			lseek(report_file, swrite, SEEK_SET);
+			write(report_file, &next, sizeof(Report));
+
+			sread += sizeof(Report);
+			swrite += sizeof(Report);
+		}
+
+		ftruncate(report_file, st.st_size - sizeof(Report));
+	}
+
+	char msg[128];
+	sprintf(msg, "User %s removed report %d from district %s\n", username, id, district);
+	write(log_file, &msg, strlen(msg));	
+}
+
+void removeDistrict() {
+	if(!checkRole("manager")) {
+		char msg[256];
+		sprintf(msg, "User %s tried to remove the district %s\n", username, district);
+		write(log_file, &msg, strlen(msg));	
+
+		return;
+	}
+
+	pid_t pid;
+	if( (pid = fork()) < 0 ) {
+		perror("Fork error on creating child for the removing a district\n");
+		return;
+	}
+
+	if(!pid) {
+		char remove[128] = "rm -drf ../districts/";
+		strcat(remove, district);
+
+		system(remove);
+
+		exit(EXIT_SUCCESS);
+	}
+
+	char msg[128];
+	sprintf(msg, "District %s removed by %s\n", district, username);
+	write(log_file, &msg, strlen(msg));	
+}
 
 
 
@@ -332,16 +670,16 @@ void handleCommand(int argc, char *argv[]) {
 	if(!strcmp("--update", command))
 		updateTreshold(argc, argv);
 	if(!strcmp("--filter", command))
-		filterReports();
+		filterReports(argc, argv);
 	if(!strcmp("--remove_report", command))
-		removeReport();
+		removeReport(argc, argv);
 	if(!strcmp("--remove_district", command))
 		removeDistrict();
 }
 
 
 int main(int argc, char *argv[]) {
-	if(argc < 7) {
+	if(argc < 5) {
 		printf("Usage: ./main --role role_name --user user_name --command command_name\n");
 		exit(EXIT_FAILURE);
 	}
