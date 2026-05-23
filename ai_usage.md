ef845012546f720b9346b54db40d1396d5d0f7b0
Alenozaurul <alenozaurul@gmail.com>
2026-05-23 12:02:12 +0300

    done for good

---
+++ ai_usage.md
@@ -0,0 +1,2 @@
+I used AI to get a better understading of the new functions.
+First I put it to explain how it's used and what is doing and then give me some examples in short programms.
--- src/city.c
+++ src/city.c
@@ -34,61 +34,76 @@ int main(int argc, char **argv) {
 
 			sleep(1);
 		} else if(!strcmp(command, "calculate_scores")) {
-			char district[32];
-			
-			printf("District: ");
-			while( (scanf("%s", district) != EOF ) && (strcmp(district, "exit") != 0) ){
-				char link[64] = "active_reports-";
-				strcat(link, district);
+			char *district = NULL;
+			char buffer[1024];
 
-				if(stat(link, &st) < 0) {
-					perror("Cannot find district\n");
-					printf("District: ");	
-					continue;
-				}
+			getchar();
+			printf("Districts: ");
+			if(fgets(buffer, sizeof(buffer), stdin) != NULL){
+				district = strtok(buffer, " ");
+				int end = 0;
 
-				pid_t pid;
-				int pfd[2];
-			
-				if(pipe(pfd) < 0){
-					perror("Pipe error on reading a district\n");
-					exit(EXIT_FAILURE);
-				}
-			
-				if( (pid = fork()) < 0 ) {
-					perror("Fork error in calculate_scores\n");
-					exit(EXIT_FAILURE);
-				}
+				while(district != NULL){ 
+					printf("%s\n", district);
 
-				if(pid == 0) {
-					close(pfd[0]);
-					dup2(pfd[1], STDOUT_FILENO);
-					close(pfd[1]);
-				
-					execl("./scorer", "scorer", link, NULL);
-						
-					perror("Execl failed on calculate_scores\n");
-					exit(1);
-				} else {
-					wait(NULL);
-					close(pfd[1]);
+					if(strchr(district, '\n')) {
+						district[strlen(district) - 1] = '\0';
+						end = 1;
+					}
+					char link[64] = "active_reports-";
+					strcat(link, district);
+					
+					district = strtok(NULL, " ");
 
-					char buffer[1024];
-					ssize_t bytes = read(pfd[0], buffer, sizeof(buffer) - 1);
-					if(bytes > 0) { 
-						buffer[bytes] = '\0';
-						printf("%s", buffer);
-					} else if(bytes == -1) {
-						perror("Couldn't read from scorer");
-						break;
-					} else if(bytes == 0) {
-						break;
+					if(stat(link, &st) < 0) {
+						printf("District not found\n");
+						//perror("Cannot find district\n");
+						continue;
 					}
 
-					close(pfd[0]);
-				}
+					pid_t pid;
+					int pfd[2];
+			
+					if(pipe(pfd) < 0){
+						perror("Pipe error on reading a district\n");
+						exit(EXIT_FAILURE);
+					}
+			
+					if( (pid = fork()) < 0 ) {
+						perror("Fork error in calculate_scores\n");
+						exit(EXIT_FAILURE);
+					}
 
-				printf("District: ");	
+					if(pid == 0) {
+						close(pfd[0]);
+						dup2(pfd[1], STDOUT_FILENO);
+						close(pfd[1]);
+				
+						execl("./scorer", "scorer", link, NULL);
+						
+						perror("Execl failed on calculate_scores\n");
+						exit(1);
+					} else {
+						wait(NULL);
+						close(pfd[1]);
+
+						char buffer[1024];
+						ssize_t bytes = read(pfd[0], buffer, sizeof(buffer) - 1);
+						if(bytes > 0) { 
+							buffer[bytes] = '\0';
+							printf("%s", buffer);
+						} else if(bytes == -1) {
+							perror("Couldn't read from scorer");
+							break;
+						} else if(bytes == 0) {
+							break;
+						}
+
+						close(pfd[0]);
+					}
+					if(end)
+						break;	
+				}
 			} 
 		} else {
 			printf("Commands are: start_monitor, calculate_scores, exit\n");
